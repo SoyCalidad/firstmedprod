34214693a45211606875288b5359949a5bf01f0e
VidalASM <vidalmsa@gmail.com>
2022-05-18 10:06:17 -0500

    journal_id in account.move view domain by user journal_ids

--- od_journal_sequence/models/account_move.py
+++ od_journal_sequence/models/account_move.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models, _
-from odoo.exceptions import ValidationError
+from odoo.exceptions import ValidationError, UserError
 from collections import defaultdict
 import logging
 
@@ -11,6 +11,50 @@ class AccountMove(models.Model):
 	_inherit = "account.move"
 
 	# name = fields.Char(compute="_compute_name_by_sequence")
+
+	@api.model
+	def _get_default_journal(self):
+		''' Get the default journal.
+		It could either be passed through the context using the 'default_journal_id' key containing its id,
+		either be determined by the default type.
+		'''
+		move_type = self._context.get('default_move_type', 'entry')
+		if move_type in self.get_sale_types(include_receipts=True):
+			journal_types = ['sale']
+		elif move_type in self.get_purchase_types(include_receipts=True):
+			journal_types = ['purchase']
+		else:
+			journal_types = self._context.get('default_move_journal_types', ['general'])
+
+		if self._context.get('default_journal_id'):
+			journal = self.env['account.journal'].browse(self._context['default_journal_id'])
+
+			if move_type != 'entry' and journal.type not in journal_types:
+				raise UserError(_(
+					"Cannot create an invoice of type %(move_type)s with a journal having %(journal_type)s as type.",
+					move_type=move_type,
+					journal_type=journal.type,
+				))
+		else:
+			journal = self._search_default_journal(journal_types)
+
+		return journal
+
+	@api.depends('company_id', 'invoice_filter_type_domain', 'move_type')
+	def _compute_suitable_journal_ids(self):
+		journals_uid = self.env.user.sale_journal_ids
+		for m in self:
+			journal_type = m.invoice_filter_type_domain or 'general'
+			company_id = m.company_id.id or self.env.company.id
+			domain = [('company_id', '=', company_id), ('type', '=', journal_type)]
+			if m.move_type == 'out_invoice' and journals_uid:
+				domain.append(('id', 'in', journals_uid.ids))
+			m.suitable_journal_ids = self.env['account.journal'].search(domain)
+
+	journal_id = fields.Many2one('account.journal', string='Journal', required=True, readonly=True,
+		states={'draft': [('readonly', False)]}, 
+		check_company=True, domain="[('id', 'in', suitable_journal_ids)]",
+		default=_get_default_journal)
 
 	@api.depends('posted_before', 'state', 'journal_id', 'date', 'l10n_latam_document_type_id', 'journal_id.is_sequence')
 	def _compute_name(self):
